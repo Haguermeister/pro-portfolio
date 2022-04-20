a758eef3dc5e3578d0c56359da3e522214bcfde2
Haguermeister <cadethague70@gmail.com>
2022-04-19 23:32:47 -0400

    Updated contact form

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,6 +3,7 @@ import React from 'react'
 import pic01 from '../images/pic01.jpeg'
 import stackOvereats from '../images/stack-overeats-screenshot.png'
 import cockyCocktails from '../images/cocky-cocktails-screenshot.png'
+import ContactForm from './contactForm'
 
 class Main extends React.Component {
   render() {
@@ -84,28 +85,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="mailto:cadethague70@gmail.com">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li>
-                <input type="submit" value="Send Message" className="special" />
-              </li>
-              <li>
-                <input type="reset" value="Reset" />
-              </li>
-            </ul>
-          </form>
+          <ContactForm />
           <ul className="icons">
             <li>
               <a href="https://www.linkedin.com/in/austinhague" className="icon fa-linkedin">
---
+++ src/components/contactForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const ContactForm = () => {
+
+  return (
+    <form>
+      <div className="field half first">
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" required />
+      </div>
+      <div className="field half">
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email" required pattern="[^@\s]+@[^@\s]+\.[^@\s]+" title="Invalid email address!" />
+      </div>
+      <div className="field">
+        <label htmlFor="message">Message</label>
+        <textarea name="message" id="message" rows="4" required></textarea>
+      </div>
+      <ul className="actions">
+        <li>
+          <input type="submit" value="Send Email" className="special" />
+        </li>
+        <li>
+          <input type="reset" value="Reset" />
+        </li>
+      </ul>
+    </form>
+  );
+};
+
+export default ContactForm;
