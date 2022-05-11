ed0532110a18f8d6051660892b49e012ef9c6cdc
Haguermeister <cadethague70@gmail.com>
2022-05-11 18:44:49 -0400

    Added houseShow

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,6 +3,8 @@ import React from 'react'
 import pic01 from '../images/pic01.jpeg'
 import stackOvereats from '../images/stack-overeats-screenshot.png'
 import cockyCocktails from '../images/cocky-cocktails-screenshot.png'
+import houseShow from '../images/HouseShow.png'
+
 import ContactForm from './contactForm'
 
 class Main extends React.Component {
@@ -24,8 +26,9 @@ class Main extends React.Component {
       >
         <article
           id="about"
-          className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''
-            }`}
+          className={`${this.props.article === 'about' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
           style={{ display: 'none' }}
         >
           <h2 className="major">About</h2>
@@ -33,46 +36,107 @@ class Main extends React.Component {
             <img src={pic01} alt="" />
           </span>
           <p>
-            Hi I'm Austin Hague. I'm currently a Civil Engineer Corps Officer for the United States Navy. In 2018 I graduated from the United States Naval Academy with a Bachelor's Degree in Robotics and Control Engineering (GPA : 3.67) while playing Varsity Football. In May of 2022, I graduated from the University of Richmond Full Stack Web Development Bootcamp.
+            Hi I'm Austin Hague. I'm currently a Civil Engineer Corps Officer
+            for the United States Navy. In 2018 I graduated from the United
+            States Naval Academy with a Bachelor's Degree in Robotics and
+            Control Engineering (GPA : 3.67) while playing Varsity Football. In
+            May of 2022, I graduated from the University of Richmond Full Stack
+            Web Development Bootcamp.
           </p>
           <p>
-            My preivous experience includes leading as a company commander of 40 United States Seabees in charge of construction equipment totaling up to $20 million. In addition, as a construction manager at the United States Naval Academy I provided construction management to a $40 million renovation project.
+            My preivous experience includes leading as a company commander of 40
+            United States Seabees in charge of construction equipment totaling
+            up to $20 million. In addition, as a construction manager at the
+            United States Naval Academy I provided construction management to a
+            $40 million renovation project.
           </p>
           <p>
-            I am a highly driven, team focused, problem solver with an engineering background who looks forward to creating a solution for you!
-            <br /><br /> By the way dont forget to checkout my work!
+            I am a highly driven, team focused, problem solver with an
+            engineering background who looks forward to creating a solution for
+            you!
+            <br />
+            <br /> By the way dont forget to checkout my work!
           </p>
           {close}
         </article>
 
         <article
           id="work"
-          className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''
-            }`}
+          className={`${this.props.article === 'work' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
           style={{ display: 'none' }}
         >
           <h2 className="major">Work</h2>
           <span className="image main">
+            <img src={houseShow} alt="" />
+          </span>
+          <h3>HouseShow</h3>
+          <p>
+            Full-Stack : MERN Stack | PWA | Bootstrap
+            <br />
+            Deployed :{' '}
+            <a
+              href="https://house-show.herokuapp.com/"
+              style={{ borderBottom: 'none' }}
+            >
+              https://house-show.herokuapp.com/
+            </a>
+            <br />
+            Github :{' '}
+            <a
+              href="https://github.com/Haguermeister/house-show"
+              style={{ borderBottom: 'none' }}
+            >
+              https://github.com/Haguermeister/house-show
+            </a>
+          </p>
+          <span className="image main">
             <img src={stackOvereats} alt="" />
           </span>
-          <h3>
-            Stack Overeats
-          </h3>
+          <h3>Stack Overeats</h3>
           <p>
-            Full-Stack : Handlebars CSS JavaScript Sequelize Bootstrap Express Node NPM Heroku<br />
-            Deployed : <a href='https://stack-overeats.herokuapp.com/' style={{ borderBottom: 'none' }}>https://stack-overeats.herokuapp.com/</a><br />
-            Github : <a href='https://github.com/Haguermeister/stack-overeats' style={{ borderBottom: 'none' }}>https://github.com/Haguermeister/stack-overeats</a>
+            Full-Stack : Handlebars | CSS | JavaScript | Sequelize | Bootstrap |
+            Express | Node | NPM | Heroku
+            <br />
+            Deployed :{' '}
+            <a
+              href="https://stack-overeats.herokuapp.com/"
+              style={{ borderBottom: 'none' }}
+            >
+              https://stack-overeats.herokuapp.com/
+            </a>
+            <br />
+            Github :{' '}
+            <a
+              href="https://github.com/Haguermeister/stack-overeats"
+              style={{ borderBottom: 'none' }}
+            >
+              https://github.com/Haguermeister/stack-overeats
+            </a>
           </p>
           <span className="image main">
             <img src={cockyCocktails} alt="" />
           </span>
-          <h3>
-            Cocky Cocktails
-          </h3>
+          <h3>Cocky Cocktails</h3>
           <p>
-            Front-End : HTML CSS JavaScript Bulma Git Github<br />
-            Deployed : <a href='https://whosshazo.github.io/cocktail-project/' style={{ borderBottom: 'none' }}>https://whosshazo.github.io/cocktail-project/</a><br />
-            Github : <a href='https://github.com/whosshazo/cocktail-project' style={{ borderBottom: 'none' }}>https://github.com/whosshazo/cocktail-project</a>
+            Front-End : HTML CSS JavaScript Bulma Git Github
+            <br />
+            Deployed :{' '}
+            <a
+              href="https://whosshazo.github.io/cocktail-project/"
+              style={{ borderBottom: 'none' }}
+            >
+              https://whosshazo.github.io/cocktail-project/
+            </a>
+            <br />
+            Github :{' '}
+            <a
+              href="https://github.com/whosshazo/cocktail-project"
+              style={{ borderBottom: 'none' }}
+            >
+              https://github.com/whosshazo/cocktail-project
+            </a>
           </p>
 
           {close}
@@ -80,15 +144,19 @@ class Main extends React.Component {
 
         <article
           id="contact"
-          className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''
-            }`}
+          className={`${this.props.article === 'contact' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
           <ContactForm />
           <ul className="icons">
             <li>
-              <a href="https://www.linkedin.com/in/austinhague" className="icon fa-linkedin">
+              <a
+                href="https://www.linkedin.com/in/austinhague"
+                className="icon fa-linkedin"
+              >
                 <span className="label">Linkedin</span>
               </a>
             </li>
@@ -103,7 +171,7 @@ class Main extends React.Component {
           </ul>
           {close}
         </article>
-      </div >
+      </div>
     )
   }
 }
